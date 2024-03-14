fcd8cfb0f8037d94980c05280b58498a6f519342
dwexman <wexmandenise@gmail.com>
2024-03-13 23:26:23 -0300

    primera tarea

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>App de Denise Wexman</Text>
+      <Text style={styles.secondaryText}>Mi primera app mobile usando React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, // Tamaño de fuente de 24 puntos
+    color: 'blue',
+  },
+  secondaryText: {
+    fontSize: 18, 
+    color: 'black', 
   },
 });
